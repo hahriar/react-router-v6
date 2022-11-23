ae4cc16ec8c670c3fd9331539236f559617db433
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-11-23 15:29:21 +0600

    problem fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CustomLink from '../CustomLink/CustomLink';
-import'../Header/Header.css'
+import React from "react";
+import CustomLink from "../CustomLink/CustomLink";
+import "../Header/Header.css";
 
 const Header = () => {
-    return (
-        <div>
-        <h1> Welcome to My Routing with header</h1>
-        <nav className='nav-bar'>
+  return (
+    <div>
+      <h1> Welcome to My Routing with header</h1>
+      <nav className="nav-bar">
         <CustomLink to="/">Home</CustomLink>
         <CustomLink to="/poster">Posts</CustomLink>
         <CustomLink to="/friends">Friends</CustomLink>
         <CustomLink to="/about">About</CustomLink>
-        </nav>
-        </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Poster = () => {
-    const [posts, setPosts] = useState([]);
-    useEffect( ()=>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => setPosts(data));
-    }, [])
-    return (
-        <div>
-            <h2> Everypost faceboook Ever had : {posts.length}</h2>
-            {
-                posts?.map(post => <Link to={`/posts/${post.id}`}>{post.id}</Link>)
-            }
-            {/* <Outlet/> */}
-        </div>
-    );
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h2> Everypost faceboook Ever had : {posts.length}</h2>
+      {posts?.map((post) => (
+        <Link key={post.id} to={`/poster/${post.id}`}>
+          {post.id}
+        </Link>
+      ))}
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Poster;
